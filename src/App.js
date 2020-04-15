f2dcdd03a75b79f40038a9d82b26a741e0e29600
awburgard <awburgard@gmail.com>
2020-04-15 07:51:46 -0500

    refreshing react

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const welcome = 'Welcome to React'
+
+class App extends React.Component {
+  state = {
+    input: 'Hello'
+  }
+
+  updateInput = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  submit = () => {
+    console.log(this.text.value)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" ref={(input) => this.text = input}/>
+        <input type="text" onChange={this.updateInput} value={this.state.input}/>
+        <button onClick={this.submit}>Show Value</button>
+        <h2>{this.state.input}</h2>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {welcome}
+          </a>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
