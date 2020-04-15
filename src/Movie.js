d43f3d0aa05382dfffe46de9dc901f3c56e8970e
awburgard <awburgard@gmail.com>
2020-04-15 08:50:28 -0500

    adds prop types

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-export default function Movie(props) {
-    return (
-        <div>
-            <h3> {props.movie.title} </h3>
-        </div>
-    )
+export default class Movie extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3> {this.props.movie.title} </h3>
+                <p>{this.props.desc}</p>
+            </div>
+        )
+    }
+}
+
+
+Movie.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+    })
+}
+
+Movie.defaultProps = {
+    desc: 'Description not avaliable'
 }
