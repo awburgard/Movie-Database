bd1bf346884caec73c558c1f403a988861759728
awburgard <awburgard@gmail.com>
2020-04-19 09:28:23 -0500

    install redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import {
   Switch,
   Link,
 } from 'react-router-dom';
+import { Provider } from 'react-redux'; 
+//  Accepts two props: 1.store, 2. children -- makes redux avaliable for your whole applicaiotn via connect()
+// connect allows us to grab props in and out of our components
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,19 +17,22 @@ import MovieDetail from './MovieDetail'
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">
-            <img src={logo} className="App-logo" alt="logo" />
-          </Link>
-        </header>
-        <Switch>
-          <Route exact path="/" component={MoviesList} />
-          <Route exact path="/:id" component={MovieDetail} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={}>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">
+              <img src={logo} className="App-logo" alt="logo" />
+            </Link>
+          </header>
+          <Switch>
+            <Route exact path="/" component={MoviesList} />
+            <Route exact path="/:id" component={MovieDetail} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+
   );
 }
 
